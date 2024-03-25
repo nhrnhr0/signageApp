bc33c57e2f562a102f3856b56f208ddd94c9ce5e
nhrnhr0 <nhrnhr0@gmail.com>
2024-03-25 02:22:32 +0200

    Update backend URLs in consts.js

--- frontend/src/lib/consts.js
+++ frontend/src/lib/consts.js
@@ -1,4 +1,7 @@
 
 
-export const BACKEND_URL = 'http://127.0.0.1:8000';
-export const BACKEND_MEDIA_URL = 'http://127.0.0.1:8000';
+// export const BACKEND_URL = 'http://127.0.0.1:8000';
+// export const BACKEND_MEDIA_URL = 'http://127.0.0.1:8000';
+
+export const BACKEND_URL = 'https://sig.ms-global.co.il';
+export const BACKEND_MEDIA_URL = 'https://sig.ms-global.co.il';
