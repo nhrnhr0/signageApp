9302414c548e0e119f7c2125ec628660c76dd446
nhrnhr0 <nhrnhr0@gmail.com>
2024-03-25 02:04:14 +0200

    Update ALLOWED_HOSTS in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8x*y3%i9l)pmk(lcw3g^ese5soo9oed30b-kkrh(7en--vx+n$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sig.ms-global.co.il',]
 
 
 # Application definition
