b0e9c703734e4b8356ce33c92c1341bb72c4f95c
nhrnhr0 <nhrnhr0@gmail.com>
2024-05-22 16:09:52 +0300

    change env lib

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,26 +11,24 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
-import environ
+from dotenv import load_dotenv,find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env = environ.Env(
-    DEBUG=(bool, False)
-)
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+load_dotenv(find_dotenv())
 
-SSO_BACKEND_URL = env('SSO_BACKEND_URL')
-FROENTEND_URL = env('FROENTEND_URL')
+
+SSO_BACKEND_URL = os.getenv('SSO_BACKEND_URL')
+FROENTEND_URL = os.getenv('FROENTEND_URL')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['sig.ms-global.co.il','127.0.0.1','home-desktop-8000.ms-global.co.il',]
 
