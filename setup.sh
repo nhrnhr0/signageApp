b41f7fab54e7163817efcde9489964ffe47f689d
nhrnhr0 <nhrnhr0@gmail.com>
2024-06-04 14:25:05 +0300

    update setup to bet aful

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
-#python3 -m venv env
+python3 -m venv env
 git pull
 source env/bin/activate
 cd backend
 pip install -r requirements.txt
 cd ..
-sudo cp setup/screen_demo1_supervisor.conf /etc/supervisor/conf.d/demo1_screen_gunicorn.conf
+sudo cp setup/demo1_screen_gunicorn.conf /etc/supervisor/conf.d/betaful_screen_gunicorn.conf
 sudo supervisorctl reread
 sudo supervisorctl update
-sudo supervisorctl restart demo1_screen_gunicorn
+sudo supervisorctl restart betaful_screen_gunicorn
+
+cd frontend
+npm install
+npm run build
+sudo cp setup/screen_demo1_nginx_django.conf /etc/nginx/conf.d/betaful_screen_nginx_django.conf
+sudo cp setup/screen_demo1_nginx_svelte.conf /etc/nginx/conf.d/betaful_screen_nginx_svelte.conf
+sudo service nginx restart
